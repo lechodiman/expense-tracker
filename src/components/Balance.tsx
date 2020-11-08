9558ac26bfb8f19e57aef19f4e3d8c31f1e897c2
Luis Chodiman <lechodiman@uc.cl>
2020-11-07 22:31:47 -0300

    chore: remove trailing whitespace

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -9,7 +9,7 @@ const Balance: React.FC = () => {
 
   return (
     <>
-      <h4 className="uppercase ">Your balance </h4>
+      <h4 className="uppercase">Your balance </h4>
       <h1 className="text-3xl tracking-wider" aria-label="total balance">
         ${total}
       </h1>
